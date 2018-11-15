ce193012629ad1e8b7508e7ed548ce6b71b0d0f4
Kai Nichols <kainichols414@gmail.com>
2018-11-15 14:38:24 -0600

    add static file serving capability

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
+from flask import render_template
+from flask import send_from_directory
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Fellow Gays!"
+    return render_template('index.html', title='Home')
+
+@app.route('/static/<path:path>')
+def download_file(filename):
+    return send_from_directory('static',filename)
