5262e4d051463558fafcee692686f1f5d4716cf3
Kai Nichols <kainichols414@gmail.com>
2018-11-15 21:56:27 -0600

    working search and filter on desktop

--- app/routes.py
+++ app/routes.py
@@ -2,14 +2,29 @@ from flask import render_template
 from flask import send_from_directory
 from geopy.geocoders import Nominatim
 from app import app
+import pandas as pd
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home')
+    df=pd.read_excel('/Users/Me/Documents/identity-quest/app/static/Identity-Quest-Demo-Resources.xlsx')
+    address=[]
+    name=[]
+    tags=[]
+    link=[]
+    service_type=[]
+    for index,row in df.iterrows():
+        address.append(row['Address'])
+        name.append(row['Organization Name'])
+        service_type.append(row['Service Type'])
+        link.append(row['Website'])
+        tags.append([i.lower() for i in row['Filter Tags'].split(", ")])
+    resources={'address':address,'name':name,'tags':tags,'type':service_type,'link':link}
+    return render_template('index.html', resources=resources,title='Home')
 
 @app.route('/map')
 def map():
+    
     address='401 Branard Street'
     geolocator = Nominatim(user_agent="Identity Quest")
     location = geolocator.geocode(address)
