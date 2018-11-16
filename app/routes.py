03be0a6ac205713387d779c51709e1cae34ee52a
Kai Nichols <kainichols414@gmail.com>
2018-11-15 18:04:57 -0600

    add header and searching

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from flask import send_from_directory
+from geopy.geocoders import Nominatim
 from app import app
 
 @app.route('/')
@@ -7,6 +8,18 @@ from app import app
 def index():
     return render_template('index.html', title='Home')
 
+@app.route('/map')
+def map():
+    address='401 Branard Street'
+    geolocator = Nominatim(user_agent="Identity Quest")
+    location = geolocator.geocode(address)
+    location = {'name':"Montrose Center",'lon':location.longitude,'lat':location.latitude}
+    return render_template('map.html',location=location, title='Map')
+
 @app.route('/static/<path:path>')
 def download_file(filename):
     return send_from_directory('static',filename)
+
+@app.route('/faq')
+def faq():
+    return render_template('index.html', title='Home')
